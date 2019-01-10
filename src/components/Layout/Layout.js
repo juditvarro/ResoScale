664ce6886ab79e64f513a0bdd920b5cb78499915
juditvarro <dr.judit.varro@gmail.com>
2019-01-10 12:38:45 +0100

    Feat(barchart): bar component and container added connected to redux store

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import Resolutions from '../../containers/Resolutions/Resolutions'
-import Resources from '../../containers/Resources/Resources';
+// import Resources from '../../containers/Resources/Resources';
+import ResourceBarContainer from '../../containers/ResourceBar/ResourcesBarContainer';
 
 class Layout extends Component {
-
-
   render() {
     return <>
       <Toolbar />
       < main className={classes.Content}>
         <Resolutions />
-        <Resources />
+        {/* <Resources /> */}
+        <ResourceBarContainer />
       </main >
     </>
   }
