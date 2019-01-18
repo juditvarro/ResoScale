06df4a785c3c0808ad2316bae987814fe6da3ca2
PKriszta <parrag.krisztina@gmail.com>
2019-01-18 09:18:49 +0100

    reducer refactoring using updateObject util

--- src/reducers/resoscale.js
+++ src/reducers/resoscale.js
@@ -1,4 +1,6 @@
-import * as actionTypes from '../actions/actionTypes'
+import * as actionTypes from '../actions/actionTypes';
+// import { createReducer } from 'redux-create-reducer';
+import { updateObject } from '../utils/utility'
 
 const initialState = {
   resources: null,
@@ -10,96 +12,90 @@ const initialState = {
 
 };
 
+const setResources = (state, action) => {
+  return updateObject(state, {
+    resources: action.resources,
+    error: false,
+  })
+}
 
+const fetchResourcesFailed = (state, action) => updateObject(state, { error: true })
+const fetchResolutionStart = (state, action) => updateObject(state, { resloading: true });
+
+const fetchResolutionSuccess = (state, action) => {
+  return updateObject(state, {
+    resolutions: action.resolutions,
+    resloading: false
+  })
+}
+const fetchResolutionFailed = (state, action) => updateObject(state, { resloading: false });
+const addResolution = (state, action) => {
+  const updatedResourcesAfterAdding = updateObject(state.resources, {
+    'brain': state.resources['brain'] + action.budgetObject['brain'],
+    'energy': state.resources['energy'] + action.budgetObject['energy'],
+    'freetime': state.resources['freetime'] + action.budgetObject['freetime'],
+    'health': state.resources['health'] + action.budgetObject['health'],
+    'money': state.resources['money'] + action.budgetObject['money'],
+    'socialvalue': state.resources['socialvalue'] + action.budgetObject['socialvalue'],
+    'willpower': state.resources['willpower'] + action.budgetObject['willpower']
+  })
+
+  const updatedAddedResolution = updateObject(state.resolutions[action.resID], { resAdded: true })
+  const updatedResolutionsAfterAdding = updateObject(state.resolutions, { [action.resID]: updatedAddedResolution })
+  const updatedStateAdd = {
+    resources: updatedResourcesAfterAdding,
+    resolutions: updatedResolutionsAfterAdding
+  }
+  return updateObject(state, updatedStateAdd)
+}
+
+const removeResolution = (state, action) => {
+  const updatedResourcesAferRemoving = updateObject(state.resources, {
+    'brain': state.resources['brain'] - action.budgetObject['brain'],
+    'energy': state.resources['energy'] - action.budgetObject['energy'],
+    'freetime': state.resources['freetime'] - action.budgetObject['freetime'],
+    'health': state.resources['health'] - action.budgetObject['health'],
+    'money': state.resources['money'] - action.budgetObject['money'],
+    'socialvalue': state.resources['socialvalue'] - action.budgetObject['socialvalue'],
+    'willpower': state.resources['willpower'] - action.budgetObject['willpower']
+  })
+
+  const updatedResolutionRemoved = updateObject(state.resolutions[action.resID], { resAdded: false })
+  const updatedResolutionsAfterRemove = updateObject(state.resolutions, { [action.resID]: updatedResolutionRemoved })
+  const updatedStateRemoval = {
+    resources: updatedResourcesAferRemoving,
+    resolutions: updatedResolutionsAfterRemove
+  }
+  return updateObject(state, updatedStateRemoval)
+}
+
+const fetchInspirationStart = (state, action) => updateObject(state, { insloading: true });
+
+const fetchInspirationSuccess = (state, action) => {
+  return updateObject(state, {
+    inspirations: action.inspirations,
+    insloading: false
+  })
+}
+
+const fetchInspirationFailed = (state, action) => updateObject(state, { insloading: false })
+
+
+// REDUCER SWITCH
 const reducer = (state = initialState, action) => {
+
+
   switch (action.type) {
-    case actionTypes.SET_RESOURCES:
-      return {
-        ...state,
-        resources: action.resources,
-        error: false,
-      }
-    case actionTypes.FETCH_RESOURCES_FAILED:
-      return {
-        ...state,
-        error: true
-      }
-    case actionTypes.FETCH_RESOLUTIONS_START:
-      return {
-        ...state,
-        resloading: true
-      }
-    case actionTypes.FETCH_RESOLUTIONS_SUCCESS:
-      return {
-        ...state,
-        resolutions: action.resolutions,
-        resloading: false
-      }
-    case actionTypes.FETCH_RESOLUTIONS_FAILED:
-      return {
-        ...state,
-        resloading: false
-      }
-    case actionTypes.ADD_RESOLUTION:
-
-      return {
-        ...state,
-        resources: {
-          ...state.resources,
-          'brain': state.resources['brain'] + action.budgetObject['brain'],
-          'energy': state.resources['energy'] + action.budgetObject['energy'],
-          'freetime': state.resources['freetime'] + action.budgetObject['freetime'],
-          'health': state.resources['health'] + action.budgetObject['health'],
-          'money': state.resources['money'] + action.budgetObject['money'],
-          'socialvalue': state.resources['socialvalue'] + action.budgetObject['socialvalue'],
-          'willpower': state.resources['willpower'] + action.budgetObject['willpower']
-        },
-        resolutions: {
-          ...state.resolutions,
-          [action.resID]: {
-            ...state.resolutions[action.resID],
-            resAdded: true
-          }
-        },
-      }
-    case actionTypes.REMOVE_RESOLUTION:
-
-      return {
-        ...state,
-        resources: {
-          ...state.resources,
-          'brain': state.resources['brain'] - action.budgetObject['brain'],
-          'energy': state.resources['energy'] - action.budgetObject['energy'],
-          'freetime': state.resources['freetime'] - action.budgetObject['freetime'],
-          'health': state.resources['health'] - action.budgetObject['health'],
-          'money': state.resources['money'] - action.budgetObject['money'],
-          'socialvalue': state.resources['socialvalue'] - action.budgetObject['socialvalue'],
-          'willpower': state.resources['willpower'] - action.budgetObject['willpower']
-        },
-        resolutions: {
-          ...state.resolutions,
-          [action.resID]: {
-            ...state.resolutions[action.resID],
-            resAdded: false
-          }
-        },
-      }
-    case actionTypes.FETCH_INSPIRATIONS_START:
-      return {
-        ...state,
-        insloading: true
-      }
-    case actionTypes.FETCH_INSPIRATIONS_SUCCESS:
-      return {
-        ...state,
-        inspirations: action.inspirations,
-        insloading: false
-      }
-    case actionTypes.FETCH_INSPIRATIONS_FAILED:
-      return {
-        ...state,
-        insloading: false
-      }
+    case actionTypes.SET_RESOURCES: return setResources(state, action)
+    case actionTypes.FETCH_RESOURCES_FAILED: return fetchResourcesFailed(state, action)
+    case actionTypes.FETCH_RESOLUTIONS_START: return fetchResolutionStart(state, action)
+    case actionTypes.FETCH_RESOLUTIONS_SUCCESS: return fetchResolutionSuccess(state, action)
+    case actionTypes.FETCH_RESOLUTIONS_FAILED: return fetchResolutionFailed(state, action)
+    case actionTypes.ADD_RESOLUTION: return addResolution(state, action)
+    case actionTypes.REMOVE_RESOLUTION: return removeResolution(state, action)
+    case actionTypes.FETCH_INSPIRATIONS_START: return fetchInspirationStart(state, action)
+    case actionTypes.FETCH_INSPIRATIONS_SUCCESS: return fetchInspirationSuccess(state, action)
+    case actionTypes.FETCH_INSPIRATIONS_FAILED: return fetchInspirationFailed(state, action)
 
     default:
       return state
